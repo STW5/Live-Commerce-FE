62d90b91e1151c7e3935d4a3de85990f90b2913f
STW5 <xodnd8384@gmail.com>
2026-02-04 21:48:15 +0900

    feat: 프로젝트 기본 설정 추가

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+};
+
+export default nextConfig;
